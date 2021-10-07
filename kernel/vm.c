9bd02a6249f4214fe013a0ecabe97e03c1b9b559
fuyuxiang <lttlstpd@live.com>
2021-10-07 11:13:59 +0800

    remove a panic

--- kernel/vm.c
+++ kernel/vm.c
@@ -180,8 +180,8 @@ uvmunmap(pagetable_t pagetable, uint64 va, uint64 npages, int do_free)
     panic("uvmunmap: not aligned");
 
   for(a = va; a < va + npages*PGSIZE; a += PGSIZE){
-    if((pte = walk(pagetable, a, 0)) == 0)
-      panic("uvmunmap: walk");
+    // if((pte = walk(pagetable, a, 0)) == 0)
+    //   panic("uvmunmap: walk");
     // if((*pte & PTE_V) == 0)
     //   panic("uvmunmap: not mapped");
     if(PTE_FLAGS(*pte) == PTE_V)
